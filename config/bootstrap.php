04590c600e94638d60bea6443514db29ca26023a
moryante1 <66669239+moryante1@users.noreply.github.com>
2026-07-22 23:13:06 +0300

    Delete config directory

--- config/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php if (isset($_SERVER['HTTP_ACCEPT_ENCODING']) && substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) { ob_start("ob_gzhandler"); } else { ob_start(); } ?>
-<?php
-if (session_status() === PHP_SESSION_NONE) { @session_start(); }
-require_once 'config.php';
-require_once 'client_config.php';
-
